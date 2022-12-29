b824cc76fa56ea02be3674a4454f3c65f184d853
Aavash <aavash604@gmail.com>
2022-12-29 20:43:37 +0545

    Student model created by Aavash

---
+++ library/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class student(models.Model):
+    first_name=models.CharField(max_length=122)
+    last_name=models.CharField(max_length=122)
+    faculty=models.CharField(max_length=122)
+    semester=models.CharField(maxlength=122)
+
+    def __str__(self):
+        return self.first_name,self.last_name

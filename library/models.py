83cd4e29742b356738ed2d9c943439d52bf0ea7c
ayyp1 <ayypsc@gmail.com>
2022-12-31 21:07:09 +0545

    dummy

--- library/models.py
+++ library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class old_ques(models.Model):
+    faculty = models.CharField(max_length=255)
+    semester = models.CharField(max_length=255)
+
+    class meta : 
+        ordering = ['year']
+
+
+

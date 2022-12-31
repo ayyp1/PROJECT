ecf98b1fd9db830e5fc555a7eead24f8b2e2708d
Aavash <aavash604@gmail.com>
2022-12-31 22:53:38 +0545

    Changes in model.py

--- library/models.py
+++ library/models.py
@@ -5,7 +5,7 @@ class student(models.Model):
     first_name=models.CharField(max_length=122)
     last_name=models.CharField(max_length=122)
     faculty=models.CharField(max_length=122)
-    semester=models.CharField(max_length=122)
+    semester=models.CharField(max_length=122,default="")
 
     def __str__(self):
         return self.first_name,self.last_name
